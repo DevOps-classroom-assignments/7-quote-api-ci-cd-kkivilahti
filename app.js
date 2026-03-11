cd8de9f95ea5cf5235cbc5d2633d511409bc1e44
kkivilahti <bhm585@myy.haaga-helia.fi>
2026-03-11 16:13:56 +0200

    Testing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/quote',(req,res)=>{
     const randomIndex = Math.floor(Math.random() * quotes.length);
-    res.json({quote: quotes[randomIndex]});
+    res.json({hello: "Hello world!", quote: quotes[randomIndex]});
 });
 
 const PORT = process.env.PORT || 3000;
